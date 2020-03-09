57f3bcbbdc2e495e9262edca349044ee32560622
kshvv975@gmail.com <RoadtoS7@users.noreply.github.com>
2020-03-09 15:58:58 +0900

    제어 연산자: takeUntil()

---
+++ src/main/java/ch4_4/TakeUntil.java
@@ -0,0 +1,23 @@
+package ch4_4;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.core.Observable;
+import reactivejava.CommonUtils;
+import reactivejava.Log;
+
+public class TakeUntil {
+	public static void run() {
+		String[] data = {"1", "2", "3", "4", "5", "6"};
+		
+		Observable<String> source = Observable.fromArray(data)
+				.zipWith(Observable.interval(100L, TimeUnit.MILLISECONDS), 
+						(val, notUsed)-> val)
+				.takeUntil(Observable.timer(500L, TimeUnit.MILLISECONDS));
+		
+		source.subscribe(Log::i);
+		CommonUtils.sleep(1000);
+		
+				
+	}
+}
